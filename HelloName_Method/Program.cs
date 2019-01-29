b9ae7295e12e2fd7005b810296b836948d3aab7b
mfribley <mfribley@mail.usf.edu>
2019-01-28 22:51:47 -0500

    Add project files.

---
+++ HelloName_Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloName_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
